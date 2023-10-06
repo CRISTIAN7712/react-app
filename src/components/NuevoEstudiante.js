88efbcb1963f538b017ae72267dbe8a0606f417e
RaptorBotDev <79489181+sebascarrera2000@users.noreply.github.com>
2023-10-05 22:58:36 -0500

    front Bot

--- src/components/NuevoEstudiante.js
+++ src/components/NuevoEstudiante.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import {Toaster,toast} from 'react-hot-toast';
 import {
     Container,
     TextField,
@@ -25,42 +26,101 @@ const NuevoEstudiante = () => {
     const [estudiante, setEstudiante] = useState(null);
     const [error, setError] = useState(null);
     const [formData, setFormData] = useState({
-        nombres: '',
-        apellidos: '',
-        identificacion: '',
-        correo: '',
+        Apellidos: '',
+        Nombres: '',
+        Identificacion: '',
+        Correo: '',
     });
 
     const handleChangeTabs = (event, newValue) => {
         setValue(newValue);
     };
 
-    const buscarEstudiante = () => {
-        if (searchId === '123') {
-            setEstudiante({
-                nombres: 'Juan',
-                apellidos: 'Pérez',
-                correo: 'juan.perez@example.com',
+    useEffect(() => {
+        console.log('Estudiante actualizado:', estudiante);
+    }, [estudiante]);
+
+    const buscarEstudiante = async () => {
+        try {
+            const response = await fetch(`http://localhost:3001/estudiantes/${searchId}`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
             });
-            setError(null);
-        } else {
-            setError('Estudiante no encontrado');
-            setEstudiante(null);
+    
+            if (response.ok) 
+            {
+                const dataEstudiante = await response.json();
+
+                if (Object.keys(dataEstudiante).length === 0) {
+                    toast.error("Estudiante no encontrado")
+                    setEstudiante(null);
+                    
+                } else {
+                    
+                    setEstudiante(dataEstudiante[0]);
+                    setError(null);
+                    toast.success('Estudiante encontrado');
+                }
+            
+            } else {
+                setError('Estudiante no encontrado');
+                
+            }
+        } catch (error) {
+            console.error('Error al buscar el estudiante:', error);
         }
     };
+    
 
     const handleChangeFormData = (e) => {
         const { name, value } = e.target;
         setFormData((prev) => ({ ...prev, [name]: value }));
     };
 
-    const handleSubmit = (e) => {
+  
+    const handleSubmit = async (e) => {
         e.preventDefault();
         console.log('Formulario enviado', formData);
+
+        let midata = JSON.stringify(formData)
+
+       
+
+        // Realizar la solicitud de creación del estudiante
+        try {
+            const response = await fetch('http://localhost:3001/estudiantes', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: midata
+                
+            });
+
+            if (response.ok) {
+                setFormData({
+                    Nombres: '',
+                    Apellidos: '',
+                    Identificacion: '',
+                    Correo: '',
+                });
+                toast.success('Estudiante Creado');
+            } else {
+                // Hubo un error al crear el estudiante
+                // Puedes manejar el error de la manera que desees
+                toast.error('Error al crear estudiante');
+            }
+        } catch (error) {
+            console.error('Error al enviar la solicitud:', error);
+        }
     };
 
     return (
         <Container component="main" maxWidth="md">
+          
+          <div><Toaster position="top-left" reverseOrder={false}/></div>
             <AppBar position="static" color="default">
                 <Tabs value={value} onChange={handleChangeTabs} variant="fullWidth">
                     <Tab label="Registrar" />
@@ -77,8 +137,8 @@ const NuevoEstudiante = () => {
                             required
                             fullWidth
                             label="Nombres"
-                            name="nombres"
-                            value={formData.nombres}
+                            name="Nombres"
+                            value={formData.Nombres}
                             onChange={handleChangeFormData}
                         />
                         <TextField
@@ -86,8 +146,8 @@ const NuevoEstudiante = () => {
                             required
                             fullWidth
                             label="Apellidos"
-                            name="apellidos"
-                            value={formData.apellidos}
+                            name="Apellidos"
+                            value={formData.Apellidos}
                             onChange={handleChangeFormData}
                         />
                         <TextField
@@ -95,8 +155,8 @@ const NuevoEstudiante = () => {
                             required
                             fullWidth
                             label="Identificación"
-                            name="identificacion"
-                            value={formData.identificacion}
+                            name="Identificacion"
+                            value={formData.Identificacion}
                             onChange={handleChangeFormData}
                         />
                         <TextField
@@ -104,8 +164,8 @@ const NuevoEstudiante = () => {
                             required
                             fullWidth
                             label="Correo Electrónico"
-                            name="correo"
-                            value={formData.correo}
+                            name="Correo"
+                            value={formData.Correo}
                             onChange={handleChangeFormData}
                         />
                         <Button
@@ -142,27 +202,27 @@ const NuevoEstudiante = () => {
 
                     {error && <Alert severity="error">{error}</Alert>}
 
-                    {estudiante && (
-                        <TableContainer component={Paper} style={{ marginBottom: '16px' }}>
-                            <Table>
-                                <TableHead>
-                                    <TableRow>
-                                        <TableCell>Nombres</TableCell>
-                                        <TableCell>Apellidos</TableCell>
-                                        <TableCell>Correo</TableCell>
-                                    </TableRow>
-                                </TableHead>
-                                <TableBody>
-                                    <TableRow>
-                                        <TableCell>{estudiante.nombres}</TableCell>
-                                        <TableCell>{estudiante.apellidos}</TableCell>
-                                        <TableCell>{estudiante.correo}</TableCell>
-                                    </TableRow>
-                                </TableBody>
-                            </Table>
-                        </TableContainer>
+                    {estudiante && ( // Verifica si estudiante tiene datos
+                    <TableContainer component={Paper} style={{ marginBottom: '16px' }}>
+                        <Table>
+                        <TableHead>
+                            <TableRow>
+                            <TableCell>Nombres</TableCell>
+                            <TableCell>Apellidos</TableCell>
+                            <TableCell>Correo</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            
+                            <TableRow>
+                            <TableCell>{estudiante.Nombres}</TableCell>
+                            <TableCell>{estudiante.Apellidos}</TableCell>
+                            <TableCell>{estudiante.Correo}</TableCell>
+                            </TableRow>
+                        </TableBody>
+                        </Table>
+                    </TableContainer>
                     )}
-
                     {estudiante && (
                         <Box>
                             <Button variant="contained" color="primary" style={{ marginRight: '8px' }}>
